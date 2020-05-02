80b3548ed00cd5af7a33cdb5bd02bbd807e46078
Jeffrey Chilberto <chilberto@yahoo.com>
2020-05-02 04:00:26 +0000

    ajax call to function added

--- web/site.js
+++ web/site.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
     $('#example').DataTable({
-        "processing": true,
-        "serverSide": true,        
-        "ajax": {
-            "url": "https://azcheapfunctionssea.azurewebsites.net/api/GetServerSideData?code=Wj9gMoAngDQH1AU0b8bvoVqNvZ8oXumOhs4hTgyZEAVyCaxQFMtfcQ==",
-            "crossDomain": true,
-            "dataType": "jsonp"
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: "https://azcheapfunctionssea.azurewebsites.net/api/GetServerSideData?code=Wj9gMoAngDQH1AU0b8bvoVqNvZ8oXumOhs4hTgyZEAVyCaxQFMtfcQ==",
+            dataType: 'json'
         }
     });
 });
