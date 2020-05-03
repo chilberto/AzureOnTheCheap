821a67ac1c36e70f5bd3722cc367aae1ce0d22ef
Jeffrey Chilberto <chilberto@yahoo.com>
2020-05-03 15:26:02 +1200

    Update siteUK.js

--- web/.js/siteUK.js
+++ web/.js/siteUK.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#example').DataTable({        
         ajax: {
-            url: "test",
+            url: "https://azcheapfunctionsuk.azurewebsites.net/api/GetServerSideData?code=6QTHUPyRvjCprL870alyRMzBElRzfIsVEVjgxoROY1U4mkRMZ8aEdA==",
             dataType: 'json'
         }
     });
