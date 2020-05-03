6bf83b47bc50e3c1aa2b762ba631571e5d9d68f1
Jeffrey Chilberto <chilberto@yahoo.com>
2020-05-03 15:26:24 +1200

    Update siteUS.js

--- web/.js/siteUS.js
+++ web/.js/siteUS.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#example').DataTable({        
         ajax: {
-            url: "tests",
+            url: "https://azcheapfunctionsus.azurewebsites.net/api/GetServerSideData?code=RUI1TR61Q7AoaTOQgP8au/iXIUbng5d1HhCogCQeJOTxqTMUzNVPgw==",
             dataType: 'json'
         }
     });
