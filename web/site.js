19024c2ec55c5fa19eb4fe5c94f6fca42b0f2c36
Jeffrey Chilberto <chilberto@yahoo.com>
2020-05-03 00:47:23 +0000

    updated to include three galleries: local images, storage images and global cdn images

--- web/site.js
+++ web/site.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-    $('#example').DataTable({
-        processing: true,
-        serverSide: true,
+    $('#example').DataTable({        
         ajax: {
             url: "https://azcheapfunctionssea.azurewebsites.net/api/GetServerSideData?code=Wj9gMoAngDQH1AU0b8bvoVqNvZ8oXumOhs4hTgyZEAVyCaxQFMtfcQ==",
             dataType: 'json'
